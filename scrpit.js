89276428ecf3b1174d57e3d67d77cb7e501ca868
sachin <sachin.sriva8@gmail.com>
2023-07-17 18:46:30 +0530

    added mouse hover effect

--- scrpit.js
+++ scrpit.js
@@ -1,35 +1,37 @@
 let parent=document.getElementById("crd");
-console.log(parent);
-parent.style.display="flex";
-parent.style.flexWrap="wrap";
-parent.style.gap="10px";
+parent.style.cssText="display: flex; flex-wrap:wrap; gap:10px;";
 
-let numbers = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105];
+let child;
+
+let numbers = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110];
 for(i=0;i<numbers.length;i++){
-    let child= document.createElement("p");
+    child= document.createElement("p");
     parent.appendChild(child);
     if(numbers[i]%2==0){
         child.innerText=numbers[i];
-        // child.style.cssText="display:flex, height:200px, width:400px, background-color:red, justify-content:center, align-item:center";
-        child.style.backgroundColor="red";
-        child.style.display="flex";
-        child.style.height="200px";
-        child.style.width="300px";
-        child.style.justifyContent="center";
-        child.style.alignItems="center";
-        child.style.fontSize="25px";
-        console.log(child);
+        var css_even="display:flex; height:200px; width:400px; background-color:red; justify-content:center; align-items:center;";
+        child.style.cssText=css_even;
     }
     if(numbers[i]%2!=0){
         child.innerText=numbers[i];
-        // child.style.cssText="display:flex, height:200, width:400, background-color:blue, justify-content:center, align-item:center";
-        child.style.backgroundColor="blue";
-        child.style.display="flex";
-        child.style.height="200px";
-        child.style.width="300px";
-        child.style.justifyContent="center";
-        child.style.alignItems="center";
-        child.style.fontSize="25px";
+        var css_odd="display:flex; height:200px; width:400px; background-color:blue; justify-content:center; align-items:center;";
+        child.style.cssText= css_odd;
     }
+
+    child.addEventListener("mouseover", (e)=>{
+        // console.log(e.target);
+        e.target.style.cssText+=`background-color:green; color:gold; transform:scale(1.1); border-radius:10px; position:relative; z-index=2;`
+    })
+    child.addEventListener("mouseout", (e)=>{
+        if(e.target.innerText%2==0){
+            e.target.style.cssText=css_even;
+        }else{
+            e.target.style.cssText=css_odd;
+        }
+    })
 }
+
+
+
+
 
